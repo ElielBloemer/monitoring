432dd0286fe0b3d2dbd4e9b63639a8ae574c79b9
Eliel Bloemer Correa <70656587+ElielBloemer@users.noreply.github.com>
2023-01-18 23:25:35 -0300

    Update mysql_exp.sh

--- mysql_exp.sh
+++ mysql_exp.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+if [[ -z $IP ]];
+then 
+     echo "NO IP passed"
+fi
+
 docker run -d --name=mysql_exporter \
 -p 9104:9104 \
--e DATA_SOURCE_NAME="root:ThePassword@(172.17.0.7:3306)/bdgrupo4" \
+-e DATA_SOURCE_NAME="root:ThePassword@($IP:3306)/bdgrupo4" \
 prom/mysqld-exporter:v0.14.0
